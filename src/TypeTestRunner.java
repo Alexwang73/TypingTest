99f0ff9e532448203407fd83f264572f7423039d
Alex Wang <alexwang6655@gmail.com>
2024-11-23 18:14:01 -0500

    Runner class

--- src/TypeTestRunner.java
+++ src/TypeTestRunner.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class TypeTestRunner {
+    public static void main(String[] args) {
+        TypeTestLogic TypeTest = new TypeTestLogic();
+        TypeTest.start();
+    }
 }

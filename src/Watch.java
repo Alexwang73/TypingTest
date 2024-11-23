cc08660c9628a137400925c5b31e38d3d87e57b5
Alex Wang <alexwang6655@gmail.com>
2024-11-23 18:16:22 -0500

    Timer class that starts a stopwatch in the background as the user types a character to start the test. Uses a thread to do this.

--- src/Watch.java
+++ src/Watch.java
@@ -1,2 +1,45 @@
-public class Watch {
+public class Watch implements Runnable {
+    //instance variables
+    private int x = 0;
+    private boolean condition;
+    private String startSequence;
+
+    //create type test logic object
+    TypeTestLogic info;
+
+    public Watch(TypeTestLogic ttl) {
+        info = ttl;
+        condition = false;
+        startSequence = info.getStart();
+        startSequence();
+    }
+
+    //check if condition is true for false in order to start the thread
+    private void startSequence() {
+        if (startSequence != "") {
+            condition = true;
+        }
+    }
+
+    //thread for timer
+    @Override
+    public void run() {
+        while (condition) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            x++;
+            System.out.println(x);
+        }
+
+    }
+
+
+    public int getX() {
+        return x;
+    }
+
+
 }

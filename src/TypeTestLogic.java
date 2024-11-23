32918e2ed7d182c244a98bb3857a56f5318433f2
Alex Wang <alexwang6655@gmail.com>
2024-11-23 18:11:10 -0500

    initial commit for TypeTestLogic class. It is responsible for managing the game loop and obtaining user input.

--- src/TypeTestLogic.java
+++ src/TypeTestLogic.java
@@ -1,2 +1,60 @@
+import java.util.Scanner;
+
 public class TypeTestLogic {
+    //instance variables
+    private Scanner scan;
+    private String info;
+    private String start;
+
+    //constructor
+    public TypeTestLogic() {
+        info = "";
+        start = "";
+        scan = new Scanner(System.in);
+    }
+
+    //game loop
+    public void start() {
+        System.out.println("Welcome to the TypingTest");
+        getInfo();
+        Watch s = new Watch(this);
+        Thread t = new Thread(s);
+        t.start();
+        System.out.println(s.getX());
+    }
+
+    //getters and setters
+
+    public String getStart() {
+        return start;
+    }
+
+    //helper methods
+    public void getInfo() {
+        System.out.println("Which test would you want to take?");
+        System.out.print("15 second test or 30 word test");
+        info = scan.nextLine();
+
+        //multiple if conditionals to load up whichever test user selects
+        /* if (info == ) {
+
+        }
+        if (info == ) {
+
+        } */
+
+        mainMenuStart();
+    }
+
+    //helper method
+    private void mainMenuStart() {
+        System.out.println("Input any character to start your test: ");
+        start = scan.nextLine();
+    }
+
+    public void printStats() {
+        System.out.println("printstats");
+    }
+
+
 }

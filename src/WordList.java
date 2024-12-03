fd4c4e7c718c85386bda7819aca8410e1a62bdcd
Alex Wang <alexw73@nycstudents.net>
2024-12-03 10:18:33 -0500

    added java doc comments to the word list class

--- src/WordList.java
+++ src/WordList.java
@@ -1,15 +1,29 @@
+/**
+ * This class generates a random word list of 15 characters
+ * from an existing word list
+ *
+ * @author Alex Wang
+ */
+
+
 public class WordList {
-    // Instance variables
+    /** The string representing the existing word list */
     private String wordList;
+
+    /** The string representing the new random word list */
     private String wordTest;
 
-    // Constructor
+    /**
+     * Instantiates a WordList object/
+     * */
     public WordList() {
         wordList = "play,one,order,would,still,number,than,around,higher,less,know,less,power,through,number,could,we,take,against,stand,call,he,with,a,so,those,if,any,say,for,be,plan,each,give,which,face,first,increase,child,after,course,much,show,the,just,line,most,form,will,under,need,one,might,become,by,find,how,against,follow,all";
         wordTest = "";
     }
 
-    // Getter for the word list
+    /**
+     * Getter method that returns the existing word list
+     * */
     public String getWordList() {
         return wordList;
     }
@@ -41,7 +55,7 @@ public class WordList {
             String word = wordList.substring(start, end);
 
             // Append the word to the test string with a comma
-            wordTest += word + ",";
+            wordTest += word + " ";
             wordCount++;
         }
 
@@ -51,7 +65,7 @@ public class WordList {
         }
     }
 
-    // Getter for the random word test
+    /** Getter method that returns the random word list of 15 characters */
     public String getWordTest() {
         return wordTest;
     }
